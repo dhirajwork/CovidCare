905d7cbb44aeb743217966ebb0e7af24afe48bed
Dhiraj Jaiswal <dhiraj.jaiswal96@gmail.com>
2020-06-26 11:47:15 +0530

    fix glitch

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -28,7 +28,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Made by Praveen & Dhiraj <3 Happy coding
+        Made by Praveen and Dhiraj
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
